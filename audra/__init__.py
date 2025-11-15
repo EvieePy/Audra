cc2b6e625673f30c5b062010e2c9f4b088d67120
EvieePy <29671945+EvieePy@users.noreply.github.com>
2025-11-15 12:08:42 +1000

    Update project metadata and build backends

--- audra/__init__.py
+++ audra/__init__.py
@@ -13,9 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-__version__ = ""
-
-
 from .application import *
 from .exceptions import *
 from .headers import *
